513a68e688c8d36df820cff1393b6c14ff399c8f
Vi Pro <VdustR@gmail.com>
2019-06-13 21:51:12 +0800

    chore: init

---
+++ poi.config.js
@@ -0,0 +1,23 @@
+const isProduction = process.env.NODE_ENV === 'production';
+
+const existFilter = a => a;
+
+module.exports = {
+  entry: [
+    'node_modules/@babel/polyfill',
+    isProduction && 'node_modules/disable-react-devtools',
+    isProduction && 'src/registerServiceWorker',
+    'src/index',
+  ].filter(existFilter),
+  plugins: [
+    {
+      resolve: '@poi/plugin-eslint',
+    },
+    {
+      resolve: '@poi/plugin-pwa',
+    },
+  ],
+  chainWebpack: config => {
+    config.resolve.alias.set('react-dom', '@hot-loader/react-dom');
+  },
+};
